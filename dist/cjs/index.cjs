28d3d07fe87b13c11d0afb776efab60406f4abe5
e22m4u <e22m4u@yandex.ru>
2025-07-03 01:22:08 +0300

    chore: replaces js-service package by inherited

--- dist/cjs/index.cjs
+++ dist/cjs/index.cjs
@@ -3,6 +3,7 @@ var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 var __export = (target, all) => {
   for (var name in all)
@@ -17,6 +18,7 @@ var __copyProps = (to, from, except, desc) => {
   return to;
 };
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
 
 // dist/esm/index.js
 var index_exports = {};
@@ -26,8 +28,233 @@ __export(index_exports, {
 module.exports = __toCommonJS(index_exports);
 
 // dist/esm/repository-data-schema.js
+var import_js_format2 = require("@e22m4u/js-format");
+
+// node_modules/@e22m4u/js-service/src/errors/invalid-argument-error.js
 var import_js_format = require("@e22m4u/js-format");
-var import_js_service = require("@e22m4u/js-service");
+var _InvalidArgumentError = class _InvalidArgumentError extends import_js_format.Errorf {
+};
+__name(_InvalidArgumentError, "InvalidArgumentError");
+var InvalidArgumentError = _InvalidArgumentError;
+
+// node_modules/@e22m4u/js-service/src/service-container.js
+var _ServiceContainer = class _ServiceContainer {
+  /**
+   * Services map.
+   *
+   * @type {Map<any, any>}
+   * @private
+   */
+  _services = /* @__PURE__ */ new Map();
+  /**
+   * Parent container.
+   *
+   * @type {ServiceContainer}
+   * @private
+   */
+  _parent;
+  /**
+   * Constructor.
+   *
+   * @param {ServiceContainer|undefined} parent
+   */
+  constructor(parent = void 0) {
+    if (parent != null) {
+      if (!(parent instanceof _ServiceContainer))
+        throw new InvalidArgumentError(
+          'The provided parameter "parent" of ServicesContainer.constructor must be an instance ServiceContainer, but %v given.',
+          parent
+        );
+      this._parent = parent;
+    }
+  }
+  /**
+   * Получить существующий или новый экземпляр.
+   *
+   * @param {*} ctor
+   * @param {*} args
+   * @return {*}
+   */
+  get(ctor, ...args) {
+    if (!ctor || typeof ctor !== "function")
+      throw new InvalidArgumentError(
+        "The first argument of ServicesContainer.get must be a class constructor, but %v given.",
+        ctor
+      );
+    if (!this._services.has(ctor) && this._parent && this._parent.has(ctor)) {
+      return this._parent.get(ctor);
+    }
+    let service = this._services.get(ctor);
+    if (!service) {
+      const ctors = this._services.keys();
+      const inheritedCtor = ctors.find((v) => v.prototype instanceof ctor);
+      if (inheritedCtor) {
+        service = this._services.get(inheritedCtor);
+        ctor = inheritedCtor;
+      }
+    }
+    if (!service || args.length) {
+      service = Array.isArray(ctor.kinds) && ctor.kinds.includes(SERVICE_CLASS_NAME) ? new ctor(this, ...args) : new ctor(...args);
+      this._services.set(ctor, service);
+    } else if (typeof service === "function") {
+      service = service();
+      this._services.set(ctor, service);
+    }
+    return service;
+  }
+  /**
+   * Проверка существования конструктора в контейнере.
+   *
+   * @param {*} ctor
+   * @return {boolean}
+   */
+  has(ctor) {
+    if (this._services.has(ctor)) return true;
+    if (this._parent) return this._parent.has(ctor);
+    const ctors = this._services.keys();
+    const inheritedCtor = ctors.find((v) => v.prototype instanceof ctor);
+    if (inheritedCtor) return true;
+    return false;
+  }
+  /**
+   * Добавить конструктор в контейнер.
+   *
+   * @param {*} ctor
+   * @param {*} args
+   * @return {this}
+   */
+  add(ctor, ...args) {
+    if (!ctor || typeof ctor !== "function")
+      throw new InvalidArgumentError(
+        "The first argument of ServicesContainer.add must be a class constructor, but %v given.",
+        ctor
+      );
+    const factory = /* @__PURE__ */ __name(() => Array.isArray(ctor.kinds) && ctor.kinds.includes(SERVICE_CLASS_NAME) ? new ctor(this, ...args) : new ctor(...args), "factory");
+    this._services.set(ctor, factory);
+    return this;
+  }
+  /**
+   * Добавить конструктор и создать экземпляр.
+   *
+   * @param {*} ctor
+   * @param {*} args
+   * @return {this}
+   */
+  use(ctor, ...args) {
+    if (!ctor || typeof ctor !== "function")
+      throw new InvalidArgumentError(
+        "The first argument of ServicesContainer.use must be a class constructor, but %v given.",
+        ctor
+      );
+    const service = Array.isArray(ctor.kinds) && ctor.kinds.includes(SERVICE_CLASS_NAME) ? new ctor(this, ...args) : new ctor(...args);
+    this._services.set(ctor, service);
+    return this;
+  }
+  /**
+   * Добавить конструктор и связанный экземпляр.
+   *
+   * @param {*} ctor
+   * @param {*} service
+   * @return {this}
+   */
+  set(ctor, service) {
+    if (!ctor || typeof ctor !== "function")
+      throw new InvalidArgumentError(
+        "The first argument of ServicesContainer.set must be a class constructor, but %v given.",
+        ctor
+      );
+    if (!service || typeof service !== "object" || Array.isArray(service))
+      throw new InvalidArgumentError(
+        "The second argument of ServicesContainer.set must be an Object, but %v given.",
+        service
+      );
+    this._services.set(ctor, service);
+    return this;
+  }
+};
+__name(_ServiceContainer, "ServiceContainer");
+var ServiceContainer = _ServiceContainer;
+
+// node_modules/@e22m4u/js-service/src/service.js
+var SERVICE_CLASS_NAME = "Service";
+var _Service = class _Service {
+  /**
+   * Container.
+   *
+   * @type {ServiceContainer}
+   */
+  container;
+  /**
+   * Constructor.
+   *
+   * @param {ServiceContainer|undefined} container
+   */
+  constructor(container = void 0) {
+    this.container = container instanceof ServiceContainer ? container : new ServiceContainer();
+  }
+  /**
+   * Получить существующий или новый экземпляр.
+   *
+   * @param {*} ctor
+   * @param {*} args
+   * @return {*}
+   */
+  getService(ctor, ...args) {
+    return this.container.get(ctor, ...args);
+  }
+  /**
+   * Проверка существования конструктора в контейнере.
+   *
+   * @param {*} ctor
+   * @return {boolean}
+   */
+  hasService(ctor) {
+    return this.container.has(ctor);
+  }
+  /**
+   * Добавить конструктор в контейнер.
+   *
+   * @param {*} ctor
+   * @param {*} args
+   * @return {this}
+   */
+  addService(ctor, ...args) {
+    this.container.add(ctor, ...args);
+    return this;
+  }
+  /**
+   * Добавить конструктор и создать экземпляр.
+   *
+   * @param {*} ctor
+   * @param {*} args
+   * @return {this}
+   */
+  useService(ctor, ...args) {
+    this.container.use(ctor, ...args);
+    return this;
+  }
+  /**
+   * Добавить конструктор и связанный экземпляр.
+   *
+   * @param {*} ctor
+   * @param {*} service
+   * @return {this}
+   */
+  setService(ctor, service) {
+    this.container.set(ctor, service);
+    return this;
+  }
+};
+__name(_Service, "Service");
+/**
+ * Kinds.
+ *
+ * @type {string[]}
+ */
+__publicField(_Service, "kinds", [SERVICE_CLASS_NAME]);
+var Service = _Service;
+
+// dist/esm/repository-data-schema.js
 var import_js_repository4 = require("@e22m4u/js-repository");
 
 // dist/esm/get-data-schema-by-model-name.js
@@ -170,7 +397,7 @@ function getDataSchemaByModelClass(dbSchema, modelClass, projectionScope) {
 __name(getDataSchemaByModelClass, "getDataSchemaByModelClass");
 
 // dist/esm/repository-data-schema.js
-var _RepositoryDataSchema = class _RepositoryDataSchema extends import_js_service.Service {
+var _RepositoryDataSchema = class _RepositoryDataSchema extends Service {
   /**
    * Get data schema by model name.
    *
@@ -179,7 +406,7 @@ var _RepositoryDataSchema = class _RepositoryDataSchema extends import_js_servic
   getDataSchemaByModelName(modelName) {
     const hasDbSchema = this.hasService(import_js_repository4.DatabaseSchema);
     if (!hasDbSchema)
-      throw new import_js_format.Errorf("A DatabaseSchema instance must be registered in the RepositoryDataSchema service.");
+      throw new import_js_format2.Errorf("A DatabaseSchema instance must be registered in the RepositoryDataSchema service.");
     return getDataSchemaByModelName(this.getService(import_js_repository4.DatabaseSchema), modelName);
   }
   /**
@@ -191,7 +418,7 @@ var _RepositoryDataSchema = class _RepositoryDataSchema extends import_js_servic
   getDataSchemaByModelClass(modelClass, projectionScope) {
     const hasDbSchema = this.hasService(import_js_repository4.DatabaseSchema);
     if (!hasDbSchema)
-      throw new import_js_format.Errorf("A DatabaseSchema instance must be registered in the RepositoryDataSchema service.");
+      throw new import_js_format2.Errorf("A DatabaseSchema instance must be registered in the RepositoryDataSchema service.");
     return getDataSchemaByModelClass(this.getService(import_js_repository4.DatabaseSchema), modelClass, projectionScope);
   }
 };
